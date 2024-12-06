33c601f3cf47cbabf0b2bf281c144e37d0a9fec5
prahulk46a <prahulk212@gmail.com>
2024-12-06 21:26:19 +0530

    Minor Updates

--- React/firstInstallationDemo/src/componets/jsx_intro/jsx_rules/jsx_rules_components/JsxRule5_Fragment.jsx
+++ React/firstInstallationDemo/src/componets/jsx_intro/jsx_rules/jsx_rules_components/JsxRule5_Fragment.jsx
@@ -3,6 +3,9 @@ let JsxRule5=()=> {
       <div >
           <>5. React Fragments are  empty angular braces.Avoiding creating extra nodes, it will improve performance.</> {/*<=fragment*/}
           {/* <Fragment>5. React Fragments are  empty angular braces.Avoiding creating extra nodes, it will improve performance</Fragment> */}
+          {/* <React.fragment>5. React Fragments are  empty angular braces.Avoiding creating extra nodes, it will improve performance</React.fragment> */}
+          {/* <React.fragment key="Str">Key can only passed with React.fragment or Fragment not with empty braces  */}
+      
       </div>
       
     )
